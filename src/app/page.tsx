c62acff0daa252da61b4207bdaba67fc0eb7aa63
Anezz12 <harsenaargrtya1@gmail.com>
2024-05-26 12:37:50 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,21 @@ import React from "react";
 
 export default function Page() {
   return (
-    <main className="text-satart pt-32 px-5 pl-16 max-w-2xl lg:max-w-5xl">
-      <div className="max-w-2xl">
-        <h1 className="text-4xl md:text-5xl font-bold mb-5">
-          Harsena Argretya
-        </h1>
-        <p className="mt-6 text-base text-zinc-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          accusamus sit, consectetur aut error quia eligendi ut ipsum quod earum
-          nobis quam unde temporibus exercitationem voluptatum accusantium eos.
-          Dolores, iure?
-        </p>
+    <main className="text-satart pt-32 px-5 ">
+      <div className="relative px-4 sm:px-8 lg:px-12">
+        <div className="mx-auto max-w-2xl lg:max-w-5xl">
+          <div className="max-w-2xl">
+            <h1 className="text-2xl md:text-5xl font-bold mb-5">
+              Harsena Argretya
+            </h1>
+            <p className="mt-6 text-base text-zinc-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              accusamus sit, consectetur aut error quia eligendi ut ipsum quod
+              earum nobis quam unde temporibus exercitationem voluptatum
+              accusantium eos. Dolores, iure?
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,27 @@
+"use client";
+import Link from "next/link";
+import { navLink } from "@/lib/data";
+
 export default function Footer() {
   return (
-    <footer className=" mt-auto text-center text-zinc-400 py-5 px-7 border-t border-zinc-800">
-      <small>&copy; 2023. All right reserved.</small>
+    <footer className=" mt-auto text-center text-zinc-400 py-5 px-7 border-t border-zinc-800 ">
+      <div className="flex flex-col  justify-between items-center">
+        <div className="flex items-center gap-5 mb-5 text-white">
+          {navLink.map((link, index) => {
+            return (
+              <Link
+                className="hover:text-cyan-400"
+                href={link.href}
+                key={index}
+                aria-label={link.name}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </div>
+        <small>&copy; 2023 Harsena Argretya All right reserved.</small>
+      </div>
     </footer>
   );
 }
