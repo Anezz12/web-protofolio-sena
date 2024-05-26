5edd3aee84f8a6022daab94b2e9f94f3b444e0b8
Anezz12 <harsenaargrtya1@gmail.com>
2024-05-26 15:22:44 +0700

    update

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function Form() {
   return (
     <section className="sm:px-8 mt-24 md:mt-28">
-      <div className=" flex-col mx-auto max-w-xl lg:max-w-none grid gap-y-10 md:gap-y-20 md:grid-cols-2 gap-5">
+      <div className="mx-auto  max-w-xl grid md:gap-y-20 gap-y-10 lg:max-w-none md:grid-cols-2 gap-10">
         <div className="space-y-10 md:space-y-0">
-          <div className="rounded-2xl border p-6 border-zinc-700/40">
+          <div className=" rounded-2xl border p-6 border-zinc-700/40">
             <p>sena</p>
           </div>
         </div>
         <div className="space-y-10 md:space-y-0">
-          <div className="rounded-2xl border p-6 border-zinc-700/40">
+          <div className=" rounded-2xl border p-6 border-zinc-700/40">
             <p>sena</p>
           </div>
         </div>
